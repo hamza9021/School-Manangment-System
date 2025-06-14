6f56bbb77be36bc838ab08a8346443ef6f7c6511
Hamza Riaz <hriaz9803@gmail.com>
2025-06-14 08:06:29 +0500

    Refactor code structure for improved readability and maintainability

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,28 +3,28 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Log in (v2)</title>
+  <title>Login</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="{{url("public/plugins/fontawesome-free/css/all.min.css")}}">
   <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="../../plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <link rel="stylesheet" href="{{url("public/plugins/icheck-bootstrap/icheck-bootstrap.min.css")}}">
   <!-- Theme style -->
-  <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="{{url("public/dist/css/adminlte.min.css")}}">
 </head>
 <body class="hold-transition login-page">
 <div class="login-box">
   <!-- /.login-logo -->
   <div class="card card-outline card-primary">
     <div class="card-header text-center">
-      <a href="../../index2.html" class="h1"><b>Admin</b>LTE</a>
+      <a href="" class="h1"><b>Admin</b>LTE</a>
     </div>
     <div class="card-body">
       <p class="login-box-msg">Sign in to start your session</p>
 
-      <form action="../../index3.html" method="post">
+      <form action="" method="post">
         <div class="input-group mb-3">
           <input type="email" class="form-control" placeholder="Email">
           <div class="input-group-append">
@@ -82,10 +82,10 @@
 <!-- /.login-box -->
 
 <!-- jQuery -->
-<script src="{{url(public/plugins/jquery/jquery.min.js)}}"></script>
+<script src="{{url("public/plugins/jquery/jquery.min.js")}}"></script>
 <!-- Bootstrap 4 -->
-<script src="{{url(public/plugins/bootstrap/js/bootstrap.bundle.min.js)}}"></script>
+<script src="{{url("public/plugins/bootstrap/js/bootstrap.bundle.min.js")}}"></script>
 <!-- AdminLTE App -->
-<script src="{{public/dist/js/adminlte.min.js}}"></script>
+<script src="{{"public/dist/js/adminlte.min.js"}}"></script>
 </body>
 </html>
